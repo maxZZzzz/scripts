c537849ed8f13024945de50537832faa17eec3c1
Reiner Lange <reiner.lange@medicalsyn.com>
2017-06-28 10:21:44 +0200

    update installer to include vim plugins

--- configs/install.sh
+++ configs/install.sh
@@ -4,12 +4,34 @@ SDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$SDIR"
 
-if [ $1 == "server" ]; then
-        TMUX_CONF=tmux/server/.tmux.conf
-else
-        TMUX_CONF=tmux/desktop/.tmux.conf  
-fi
+install_tmux() {
+        if [ "$1" == "server" ]; then
+                TMUX_CONF=tmux/server/.tmux.conf
+        else
+                TMUX_CONF=tmux/desktop/.tmux.conf  
+        fi
 
-cp $TMUX_CONF ~/
-rsync -vaAHD vim/ ~/
+        cp $TMUX_CONF ~/
+}
 
+install_vim() {
+        rsync -vaAHD vim/ ~/
+
+        cd ~/.vim/bundle
+        git clone git://github.com/altercation/vim-colors-solarized.git
+        cd vim-colors-solarized
+        git pull
+
+        cd ..
+        git clone https://github.com/terryma/vim-multiple-cursors
+        cd vim-multiple-cursors
+        git pull
+
+        cd ..
+        git clone git://github.com/tpope/vim-sensible.git
+        cd vim-sensible
+        git pull
+}
+
+install_tmux "$@"
+install_vim "$@"
