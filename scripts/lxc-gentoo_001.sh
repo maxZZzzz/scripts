9a8269b6b0b8c1560dfad2fe746f24700a25b793
Reiner Lange <reiner.lange@gmx.de>
2018-01-25 13:30:42 +0100

    updated lxc default config

--- scripts/lxc-gentoo_001.sh
+++ scripts/lxc-gentoo_001.sh
@@ -1,47 +1,40 @@
 #!/bin/bash
 
 # config
-#SSH_KEY=''
-HOST_NUMBER='5'
+
 
 # script
-
-cat > /etc/conf.d/net <<EOF
+configure_net() {
+    cat > /etc/conf.d/net <<EOF
 modules="dhcpcd"
 
-config_eth0="dhcp fd00::$HOST_NUMBER/64"
-dhcpcd_eth0="-6"
-routes_eth0="fd00::/64"
+config_eth0="10.2.55.${HOST_NUMBER}
+fd00::${HOST_NUMBER}/64"
+routes_eth0="10.2.0.0/16
+fd00::/64
+default via 10.2.0.1"
 EOF
 
-cd /etc/init.d
-ln -s net.lo net.eth0
-rc-update add net.eth0 boot
-rc-update add sshd boot
+    cd /etc/init.d
+    ln -s net.lo net.eth0
+    rc-update add net.eth0 boot
+    rc-update add sshd boot
 
-cd /root
-mkdir .ssh
-chmod go= .ssh
-cd .ssh
-echo "${SSH_KEY}" > authorized_keys
+    cd /root
+    mkdir .ssh
+    chmod go= .ssh
+    cd .ssh
+    echo "${SSH_KEY}" > authorized_keys
+    
+    /etc/init.d/net.eth0 restart
+}
 
-echo 'USE="$USE bash-completion"' >> /etc/portage/make.conf
-echo 'MAKEOPTS="-j4"' >> /etc/portage/make.conf
-emerge app-misc/mc vim tmux bash-completion dhcpcd gentoolkit htop dev-vcs/git
-service net.eth0 restart
+configure_profile() {
+    eselect profile set default/linux/amd64/17.0
 
-echo 'set-option -g prefix C-a
-bind a send-prefix
-
-set-option -g mouse on
-
-# Active pane border colour
-set-option -g pane-active-border-fg yellow
-set -g default-terminal "screen-256color"
-' > /root/.tmux.conf
-
-mkdir /root/bin
-echo '#!/bin/bash
+    mkdir /root/bin
+    cat > /root/bin/update-system.sh <<EOF
+#!/bin/bash
 
 
 set -x
@@ -56,5 +49,56 @@ emerge --depclean
 revdep-rebuild
 emerge @preserved-rebuild
 eclean-dist
-' > /root/bin/update-system.sh
-chmod +x /root/bin/update-system.sh
+EOF
+    chmod +x /root/bin/update-system.sh
+
+    /root/bin/update-system.sh nosync
+}
+
+configure_shell() {
+    
+    local content="$(cat /etc/portage/make.conf | sed -e 's/^USE="bindist"$/#USE="bindist/')"
+    echo "$content" > /etc/portage/make.conf
+    echo 'USE="$USE bash-completion"' >> /etc/portage/make.conf
+    echo 'MAKEOPTS="-j4"' >> /etc/portage/make.conf
+    emerge app-misc/mc vim tmux bash-completion gentoolkit htop dev-vcs/git netcat
+    
+    cat >/root/.tmux.conf <<EOF
+set-option -g prefix C-a
+bind a send-prefix
+
+set-option -g mouse on
+
+# Active pane border colour
+set-option -g pane-active-border-fg yellow
+EOF
+
+}
+
+configure_avahi() {
+    emerge net-dns/avahi sys-auth/nss-mdns
+    rc-update add avahi-daemon default
+    local content=$(cat /etc/nsswitch.conf  | sed -e 's/\(hosts:.*\)/\1 mdns/')
+    echo  "$content" > /etc/nsswitch.conf
+}
+
+configure_cron() {
+    echo 'mail-mta/exim -X maildir mbx syslog' > /etc/portage/package.use/exim
+    echo 'mail-client/mutt doc gpg gpgme mbox smime hcache idn' > /etc/portage/package.use/mutt
+    
+    emerge mutt exim vixie-cron
+    
+    rc-update add exim
+    rc-update add vixie-cron
+    
+    local content="$(cat /etc/crontab | sed -e 's/MAILTO=root/MAILTO=admin/')"
+    echo "$content" > /etc/crontab
+}
+
+configure_net
+configure_profile
+configure_shell
+configure_avahi
+configure_cron
+
+openrc
